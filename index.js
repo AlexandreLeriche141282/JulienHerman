f9322fc7145ee2c04753772200b628c5747f8bf2
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2023-12-22 19:42:47 +0100

    addImg

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const textVisible1 = document.querySelector(".titre1");
 const textVisible2 = document.querySelector(".titre2");
 const textVisible3 = document.querySelector(".titre3");
 const textVisible4 = document.querySelector(".titre4");
+const textVisible5 = document.querySelector(".titre5");
+const textVisible6 = document.querySelector(".titre6");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY < lastScroll) {
@@ -19,7 +21,7 @@ window.addEventListener("scroll", () => {
  
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.70) {
+    if (scrollValue > 0.60) {
         
         textVisible1.style.opacity = "1"
         textVisible1.style.top = "45%"
@@ -30,7 +32,7 @@ window.addEventListener("scroll", () => {
 
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.70) {
+    if (scrollValue > 0.60) {
         
         textVisible2.style.opacity = "1"
         textVisible2.style.top = "45%"
@@ -40,7 +42,7 @@ window.addEventListener("scroll", () => {
 });
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.90) {
+    if (scrollValue > 0.75) {
         
         textVisible3.style.opacity = "1"
         textVisible3.style.top = "45%"
@@ -50,10 +52,30 @@ window.addEventListener("scroll", () => {
 });
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.90) {
+    if (scrollValue > 0.75) {
         
         textVisible4.style.opacity = "1"
         textVisible4.style.top = "45%"
+        
+    }
+    
+});
+window.addEventListener("scroll", () => {
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    if (scrollValue > 0.95) {
+        
+        textVisible5.style.opacity = "1"
+        textVisible5.style.top = "45%"
+        
+    }
+    
+});
+window.addEventListener("scroll", () => {
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    if (scrollValue > 0.95) {
+        
+        textVisible6.style.opacity = "1"
+        textVisible6.style.top = "45%"
         
     }
     
