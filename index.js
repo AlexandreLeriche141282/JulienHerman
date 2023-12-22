e29896683698d7da5f88c698ee6f7f3b918fd4ef
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2023-12-22 16:54:55 +0100

    text+scrollEvent

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let lastScroll = 0;
 const menu = document.querySelector(".menu");
-const textChoux = document.getElementById("textChoux")
-const textFinger=document.getElementById("textFinger")
+const textVisible1 = document.querySelector(".titre1");
+const textVisible2 = document.querySelector(".titre2");
+const textVisible3 = document.querySelector(".titre3");
+const textVisible4 = document.querySelector(".titre4");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY < lastScroll) {
@@ -11,22 +13,49 @@ window.addEventListener("scroll", () => {
     }
     lastScroll = window.scrollY;
 
-  });
+});
+  
+    
+ 
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.90) {
-        textChoux.style.visibility = "visible"
-        textChoux.style.opacity = "1"
-        textChoux.style.top = "20%"
+    if (scrollValue > 0.70) {
+        
+        textVisible1.style.opacity = "1"
+        textVisible1.style.top = "45%"
+        
+    }
+    
+});
+
+window.addEventListener("scroll", () => {
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    if (scrollValue > 0.70) {
+        
+        textVisible2.style.opacity = "1"
+        textVisible2.style.top = "45%"
+        
     }
     
 });
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
     if (scrollValue > 0.90) {
-        textFinger.style.visibility = "visible"
-        textFinger.style.opacity = "1"
-        textFinger.style.top = "20%"
+        
+        textVisible3.style.opacity = "1"
+        textVisible3.style.top = "45%"
+        
     }
     
-    });
+});
+window.addEventListener("scroll", () => {
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    if (scrollValue > 0.90) {
+        
+        textVisible4.style.opacity = "1"
+        textVisible4.style.top = "45%"
+        
+    }
+    
+});
+
