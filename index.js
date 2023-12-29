80de5523a02212ea2076e83af643a3ab2b5d9b5e
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2023-12-29 23:10:33 +0100

    EmailJs pour formulaire

--- index.js
+++ index.js
@@ -9,97 +9,111 @@ const textVisible6 = document.querySelector(".titre6");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY < lastScroll) {
-        menu.style.top=0;
+        menu.style.top = 0;
     } else {
-    menu.style.top="-140px"
+        menu.style.top = "-140px"
     }
     lastScroll = window.scrollY;
 
 });
-  
-    
- 
+
+
+
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
     if (scrollValue > 0.60) {
-        
+
         textVisible1.style.opacity = "1"
         textVisible1.style.top = "45%"
-        
+
     } else {
         textVisible1.style.opacity = "0"
-        textVisible1.style.top = "75%" 
+        textVisible1.style.top = "75%"
     }
-    
+
 });
 
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
     if (scrollValue > 0.60) {
-        
+
         textVisible2.style.opacity = "1"
         textVisible2.style.top = "45%"
-        
+
     } else {
         textVisible2.style.opacity = "0"
         textVisible2.style.top = "75%"
     }
-    
+
 });
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
     if (scrollValue > 0.75) {
-        
+
         textVisible3.style.opacity = "1"
         textVisible3.style.top = "45%"
-        
+
     } else {
         textVisible3.style.opacity = "0"
         textVisible3.style.top = "75%"
     }
-    
+
 });
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
     if (scrollValue > 0.75) {
-        
+
         textVisible4.style.opacity = "1"
         textVisible4.style.top = "45%"
-        
+
     } else {
         textVisible4.style.opacity = "0"
         textVisible4.style.top = "75%"
     }
-    
+
 });
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
     if (scrollValue > 0.95) {
-        
+
         textVisible5.style.opacity = "1"
         textVisible5.style.top = "45%"
-        
+
     } else {
         textVisible5.style.opacity = "0"
         textVisible5.style.top = "75%"
     }
-    
+
 });
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
     if (scrollValue > 0.95) {
-        
+
         textVisible6.style.opacity = "1"
         textVisible6.style.top = "45%"
-        
+
     } else {
         textVisible6.style.opacity = "0"
         textVisible6.style.top = "75%"
     }
-    
+
 });
 
 function togglePopup() {
     let popup = document.querySelector("#popup-overlay")
     popup.classList.toggle("active");
 }
+
+function sendMail() {
+    let params = {
+        name: document.getElementById("name").value,
+        sujet: document.getElementById("sujet").value,
+        email: document.getElementById("email").value,
+        telephone: document.getElementById("telephone").value,
+        message: document.getElementById("message").value,
+
+    }
+
+    emailjs.send("service_86vpx3l", "template_2nhdv2u", params).then(alert("Votre message nous a bien été transmis"))
+}
+
