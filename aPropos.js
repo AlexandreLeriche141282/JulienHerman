95e9cfa29ce6df1cda263aced5149d39ca945907
alexandreleriche141282 <alexandreleriche141282@gmail.com>
2024-01-12 16:58:43 +0100

    modif

--- aPropos.js
+++ aPropos.js
@@ -1,64 +1,23 @@
 const img1 = document.querySelector(".img1");
 const img2 = document.querySelector(".img2");
-const menu = document.querySelector(".menu");
-let lastScroll = 0;
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY < lastScroll) {
-        menu.style.top = 0;
-    } else {
-        menu.style.top = "-140px"
-    }
-    lastScroll = window.scrollY;
+// ---------- Apparition Photos ------------------ //
 
-});
 
 window.addEventListener("scroll", () => {
     let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
     if (scrollValue > 0.70) {
 
         img1.style.width = "80%"
-        
+        img2.style.width = "80%"
 
     } else {
         img1.style.width = "50%"
-       
-    }
-
-});
-window.addEventListener("scroll", () => {
-    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.70) {
-
-        img2.style.width = "80%"
-        
-
-    } else {
         img2.style.width = "50%"
-       
     }
 
 });
 
-// --------------------------------------------------------------------------------
-// -----------------Effet toggle sur formulaire Popup----------------------------//
-function togglePopup() {
-    let popup = document.querySelector("#popup-overlay")
-    popup.classList.toggle("active");
-}
+// -------------------------------------------------//
 
-// --------------------------------------------------------------------------------
-// -----------------Fonction pour envoi de formulaire via EmailJs-----------------//
-function sendMail() {
-    let params = {
-        name: document.getElementById("name").value,
-        sujet: document.getElementById("sujet").value,
-        email: document.getElementById("email").value,
-        telephone: document.getElementById("telephone").value,
-        message: document.getElementById("message").value,
 
-    }
-
-    emailjs.send("service_86vpx3l", "template_2nhdv2u", params).then(alert("Votre message nous a bien été transmis"))
-}
-// ------------------------------------------------------------------------------------
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // -------------- Scroll navBar --------------//
+const menu = document.querySelector(".menu");
+let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
     if (window.scrollY < lastScroll) {
@@ -12,120 +14,52 @@ window.addEventListener("scroll", () => {
 
 
 // --------------ApparitiontextePhotoscroll-----------------// 
-let lastScroll = 0;
-let playOnce = true;
+const sr = ScrollReveal();
 
-const menu = document.querySelector(".menu");
-const textVisible1 = document.querySelector(".titre1");
-const textVisible2 = document.querySelector(".titre2");
-const textVisible3 = document.querySelector(".titre3");
-const textVisible4 = document.querySelector(".titre4");
-const textVisible5 = document.querySelector(".titre5");
-const textVisible6 = document.querySelector(".titre6");
-
-const ratio = 0.1
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: ratio
-}
-
-const handleIntersect = function (entries, observer) {
-    entries.forEach(function (entry){
-        if (entry.intersectionRatio > ratio) {
-            {
-
-                textVisible1.style.opacity = "1"
-                textVisible1.style.top = "45%"
-        
-            }
-       } 
-    })
-}
-
-
-
-
-window.addEventListener("scroll", () => {
-    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.35 ) {
-
-        textVisible1.style.opacity = "1"
-        textVisible1.style.top = "45%"
-
-    } else {
-        textVisible1.style.opacity = "0"
-        textVisible1.style.top = "75%"
-    }
-
+sr.reveal(".titre1", {
+    duration: 1000,
+    origin: 'top',
+    distance: '-200px',
+    scale: '0.5',
+    reset:'true'
+});
+sr.reveal(".titre2", {
+    duration: 1000,
+    origin: 'top',
+    distance: '-200px',
+    scale: '0.5',
+    reset:'true'
+});
+sr.reveal(".titre3", {
+    duration: 1000,
+    origin: 'top',
+    distance: '-200px',
+    scale: '0.5',
+    reset:'true'
+});
+sr.reveal(".titre4", {
+    duration: 1000,
+    origin: 'top',
+    distance: '-200px',
+    scale: '0.5',
+    reset:'true'
+});
+sr.reveal(".titre5", {
+    duration: 1000,
+    origin: 'top',
+    distance: '-200px',
+    scale: '0.5',
+    reset:'true'
+});
+sr.reveal(".titre6", {
+    duration: 1000,
+    origin: 'top',
+    distance: '-200px',
+    scale: '0.5',
+    reset:'true'
 });
 
-window.addEventListener("scroll", () => {
-    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.35) {
 
-        textVisible2.style.opacity = "1"
-        textVisible2.style.top = "45%"
-
-    } else {
-        textVisible2.style.opacity = "0"
-        textVisible2.style.top = "75%"
-    }
-
-});
-window.addEventListener("scroll", () => {
-    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.50) {
-
-        textVisible3.style.opacity = "1"
-        textVisible3.style.top = "45%"
-
-    } else {
-        textVisible3.style.opacity = "0"
-        textVisible3.style.top = "75%"
-    }
-
-});
-window.addEventListener("scroll", () => {
-    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.50) {
-
-        textVisible4.style.opacity = "1"
-        textVisible4.style.top = "45%"
-
-    } else {
-        textVisible4.style.opacity = "0"
-        textVisible4.style.top = "75%"
-    }
-
-});
-window.addEventListener("scroll", () => {
-    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.60) {
-
-        textVisible5.style.opacity = "1"
-        textVisible5.style.top = "45%"
-
-    } else {
-        textVisible5.style.opacity = "0"
-        textVisible5.style.top = "75%"
-    }
-
-});
-window.addEventListener("scroll", () => {
-    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
-    if (scrollValue > 0.60) {
-
-        textVisible6.style.opacity = "1"
-        textVisible6.style.top = "45%"
-
-    } else {
-        textVisible6.style.opacity = "0"
-        textVisible6.style.top = "75%"
-    }
-
-});
-// --------------------------------------------------------------------------------
 // -----------------Effet toggle sur formulaire Popup----------------------------//
 function togglePopup() {
     let popup = document.querySelector("#popup-overlay")
@@ -175,6 +109,22 @@ croix.addEventListener("click", () => {
     ulMenu.classList.toggle('mobileMenu')
     
 });
+
+window.addEventListener("scroll", () => {
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    if (scrollValue > 0.70) {
+
+        img1.style.width = "80%"
+        
+
+    } else {
+        img1.style.width = "50%"
+       
+    }
+
+});
+
+
 
 
 
